e7bd4a771f42085dfaa84efa415f749b658f87f1
dartherodotywf <168344250+dartherodotywf@users.noreply.github.com>
2024-04-28 09:03:55 -0300

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-50427f0c-5636-4b4c-bf9f-fcbeb9ee5063/"
 
 OUTPUT_DIR="logs"
 
